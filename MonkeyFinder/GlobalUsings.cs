f7c3c4fdcd5e629c851117a19f8a723c2b7eda08
DamiánQuezada <damianquezada.s@gmail.com>
2023-06-22 15:18:19 -0400

    Completed MVVM Module

--- MonkeyFinder/GlobalUsings.cs
+++ MonkeyFinder/GlobalUsings.cs
@@ -1,7 +1,7 @@
 ﻿global using CommunityToolkit.Mvvm.ComponentModel;
 global using CommunityToolkit.Mvvm.Input;
 global using MonkeyFinder.Model;
-//global using MonkeyFinder.ViewModel;
+global using MonkeyFinder.ViewModel;
 global using System.Collections.ObjectModel;
 global using System.ComponentModel;
 global using System.Diagnostics;
--- MonkeyFinder/ViewModel/BaseViewModel.cs
+++ MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -1,6 +1,19 @@
 ﻿namespace MonkeyFinder.ViewModel
 {
-	public class BaseViewModel
+	public partial class BaseViewModel : ObservableObject
 	{
+		public BaseViewModel()
+		{
+
+		}
+
+		[ObservableProperty]
+		private string _title;
+
+		[ObservableProperty]
+		[NotifyPropertyChangedFor(nameof(IsNotBusy))]
+		private bool _isBusy;
+
+		public bool IsNotBusy => !IsBusy;
 	}
 }
